71423ba2cff1ccfbc465d7842c9d3852e2741a80
Altmertrick <altmertricks@gmail.com>
2023-02-17 09:23:22 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      Hello world
+      <button className="button is-link">add</button>
+    </div>
+  );
+}
+
+export default App;
