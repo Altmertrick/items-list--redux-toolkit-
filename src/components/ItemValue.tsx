7b05aae09f478e2411f4d075029eb859c707867b
Altmertrick <altmertricks@gmail.com>
2023-02-18 08:55:28 +0200

    ItemValue added calculation of total cost

--- src/components/ItemValue.tsx
+++ src/components/ItemValue.tsx
@@ -1,7 +1,19 @@
 type PropsT = {};
+import { useSelector } from 'react-redux';
+import { RootState } from '../state';
 
 const ItemValue: React.FC<any> = () => {
-  return <div>ItemValue</div>;
+  const itemsTotalCost = useSelector(
+    ({ items: { itemsData, searchTerm } }: RootState) => {
+      return itemsData
+        .filter((item) =>
+          item.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+        )
+        .reduce((acc, item) => acc + item.cost, 0);
+    }
+  );
+
+  return <div className="items-value">Total cost ${itemsTotalCost}</div>;
 };
 
 export default ItemValue;
